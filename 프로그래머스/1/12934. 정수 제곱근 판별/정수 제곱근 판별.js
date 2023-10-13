3915d688f3c75922049cd9b5cc035d02ccf74639
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2023-10-13 09:55:53 +0900

    Update 정수 제곱근 판별.js

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,18 +1,7 @@
 function solution(n) {
     let result = 0;
+    let a = 0;
     result = Math.sqrt(n)
-    if (result % 1 === 0) {
-        return Math.pow(result + 1, 2);
-    } else {
-        return -1;
-    }
+    result % 1 === 0 ?  a = Math.pow(result + 1, 2) : a = -1;
+    return a;
 }
-
-
-
-
-
-
-
-
-         
