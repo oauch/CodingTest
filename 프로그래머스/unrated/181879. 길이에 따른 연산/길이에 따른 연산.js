8fe6af95bafe4b4f8030863f1611ed9b878fc825
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2023-10-16 15:55:43 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let answer = 0;
+    if (num_list.length >= 11) {
+        for (let i = 0; i < num_list.length; i++) {
+            answer += num_list[i];
+        }
+    } else {
+        answer = 1;
+        for (let i = 0; i < num_list.length; i++) {
+            answer *= num_list[i];
+        }
+    }
+    return answer;
+}
