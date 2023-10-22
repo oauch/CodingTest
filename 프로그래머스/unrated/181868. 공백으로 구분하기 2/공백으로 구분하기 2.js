bd2f257e9a6920fc6448a5f54bdbed3c54f4d2c6
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2023-10-22 16:43:48 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,6 +1,3 @@
 function solution(my_string) {
-    let result = [];
-    let string = my_string.split(' ');
-    string.map((word) => word !== '' ? result.push(word) : word);
-    return result;
+    return my_string.split(' ').filter(word => word);
 }
