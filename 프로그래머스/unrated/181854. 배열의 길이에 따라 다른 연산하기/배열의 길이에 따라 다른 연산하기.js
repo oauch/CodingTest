bbefc0580484987d6e40b14d2596a286cbd71ca7
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2023-10-25 20:32:11 +0900

    ho..

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,21 +1,36 @@
 function solution(arr, n) {
     let answer = [];
-    if (arr.length % 2 === 0) {
-        for (let i in arr) {
-            if (i % 2 !== 0) {
-                answer.push(arr[i] + n);
-            } else {
-                answer.push(arr[i]);
-            }
-        }
+
+    if(arr.length%2===0) {
+        answer = arr.map((v,i)=>i%2 === 1 ? v + n : v);
     } else {
-         for (let i in arr) {
-            if (i % 2 === 0) {
-                answer.push(arr[i] + n);
-            } else {
-                answer.push(arr[i]);
-            }
-        }
+        answer = arr.map((v,i)=>i%2 === 0 ? v + n : v);
     }
+
+
     return answer;
 }
+
+
+
+// function solution(arr, n) {
+//     let answer = [];
+//     if (arr.length % 2 === 0) {
+//         for (let i in arr) {
+//             if (i % 2 !== 0) {
+//                 answer.push(arr[i] + n);
+//             } else {
+//                 answer.push(arr[i]);
+//             }
+//         }
+//     } else {
+//          for (let i in arr) {
+//             if (i % 2 === 0) {
+//                 answer.push(arr[i] + n);
+//             } else {
+//                 answer.push(arr[i]);
+//             }
+//         }
+//     }
+//     return answer;
+// }
