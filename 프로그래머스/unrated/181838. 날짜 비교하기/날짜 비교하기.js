11f9ff650a87a5b6b9e03b729c2de945ebc55b05
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2023-10-26 10:57:02 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.09 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,9 +1,3 @@
 function solution(date1, date2) {
-    const [year1, month1, day1] = date1;
-    const [year2, month2, day2] = date2;
-    
-    if (year1 !== year2) return year1 < year2 ? 1 : 0;
-    if (month1 !== month2) return month1 < month2 ? 1 : 0;
-    if (day1 !== day2) return day1 < day2 ? 1 : 0;
-    return 0;
+    return new Date(...date1) < new Date(...date2) ? 1 : 0;
 }
