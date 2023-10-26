fe41c3dc83e664d441c5432fbd71504a05e05f13
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2023-10-26 10:16:28 +0900

    Update 홀수 vs 짝수.js

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -2,9 +2,5 @@ function solution(num_list) {
     let even = 0;
     let odd = 0;
     num_list.map((num, i) => i % 2 === 0 ? even += num : odd += num)
-    if (even > odd) {
-        return even
-    } else {
-        return odd;
-    }
+    return even > odd ? even : odd;
 }
