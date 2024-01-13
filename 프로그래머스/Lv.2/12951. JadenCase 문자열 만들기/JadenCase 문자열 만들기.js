c3c54a457def57d0033f7ccca9b1fa1209e1eda3
YoonHyuk Lim <49686619+oauch@users.noreply.github.com>
2024-01-13 19:09:10 +0900

    [level Lv.2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,13 +1,3 @@
-// 처음 시도할 때
-// function solution(s) {
-//     const test = s.split(' ').map((val) => val.toLowerCase());
-//     return test.map((val) => val[0].toUpperCase() + val.slice(1)).join(' ');
-// }
-
-
 function solution(s) {
-    return s.split(' ').map(sentence => sentence.replace(/\w/g, (t,idx)=>{
-        return idx === 0 ? t.toUpperCase() : t.toLowerCase()
-    })).join(' ')
-
+    return s.split(' ').map((val) => val.charAt(0).toUpperCase() + val.slice(1).toLowerCase()).join(' ');
 }
