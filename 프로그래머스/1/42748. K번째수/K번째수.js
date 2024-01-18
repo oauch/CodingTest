25073c681b9d8aaeb3da130fde01273f239bdbc4
YoonHyuk Lim <dkssud4930@gmail.com>
2024-01-18 10:41:36 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,7 @@
 function solution(array, commands) {
-    const result = [];
-    for (const cmd of commands) {
-        const [start, end, k] = cmd;
-        const slicedArray = array.slice(start - 1, end);
-        const sortedArray = slicedArray.sort((a, b) => a - b);
-        const kthElement = sortedArray[k - 1];
-        result.push(kthElement);
+    const result = []
+    for (i of commands) {
+        result.push(array.slice(i[0]-1, i[1]).sort((a, b) => a - b)[i[2]-1]);
     }
     return result;
 }
