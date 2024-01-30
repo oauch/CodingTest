a8258daafbcf19d54a9eb7e2365d6aa937725fa4
YoonHyuk Lim <dkssud4930@gmail.com>
2024-01-30 16:24:33 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -1,6 +1,6 @@
 function solution(board, k) {
     let answer = 0;
-    board.map((val, index) => val.filter((val2, index2) => index + index2 <= k && 
+    board.map((val, index) => val.map((val2, index2) => index + index2 <= k && 
                                       (answer += board[index][index2])));
     return answer;
 }
