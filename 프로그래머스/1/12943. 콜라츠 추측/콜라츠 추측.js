2ddb53e5ffa99bc14ef881fcaef52d3eb22f519a
YoonHyuk Lim <dkssud4930@gmail.com>
2024-03-05 12:28:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    let count = 0;
+    while (num !== 1) {
+        num % 2 === 0 ? num /= 2 : num = num * 3 + 1;
+        count += 1;
+        if (count === 500) return -1;
+    }
+    return count;
+}
