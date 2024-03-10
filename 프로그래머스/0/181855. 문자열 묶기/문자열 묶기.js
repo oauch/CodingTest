7318722991dcd7e86cfc9083d415c04474b3a47b
YoonHyuk Lim <dkssud4930@gmail.com>
2024-03-10 20:09:28 +0900

    [level 0] Title: 문자열 묶기, Time: 8.17 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,15 +1,6 @@
-function solution(strArr) {
-    
-    const a = strArr.map((val) => val.length);
-    const result = {}
-    a.forEach((val) => result[val] = (result[val] || 0) + 1);
-    
-    return Math.max(...Object.values(result));
-    
-    // 실패케이스
-    // for (let i of strArr) {
-    //     answer.push(i.length);
-    //     result.push(answer.sort().map((val) => val === i.length).filter((val) => val).length);
-    // }
-    // return Math.max(...result);
+function solution(입력) {
+    const 길이 = 입력.map((val) => val.length);
+    const 길이의_갯수 = {};
+    길이.map((val) => (길이의_갯수[val] = (길이의_갯수[val] ?? 0) + 1))
+    return Math.max(...Object.values(길이의_갯수));
 }
